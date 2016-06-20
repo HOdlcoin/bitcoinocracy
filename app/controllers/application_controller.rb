ad153f41abf95b7ead6a29fd501a72b477f5c430
saddaqayyum57 <saddaqayyum@gmail.com>
2016-06-20 04:03:48 -0400

    null case handled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,10 @@ class ApplicationController < ActionController::Base
 
     if BitcoinAddress.where('updated_at > ?',10.seconds.ago).empty? # don't DOS blockchain.info
       bitcoin_address = BitcoinAddress.unscoped.order("updated_at ASC").first
-      if bitcoin_address.updated_at < 10.minutes.ago # no need to do it too often
-        bitcoin_address.update_balance
+      if bitcoin_address
+        if bitcoin_address.updated_at < 10.minutes.ago # no need to do it too often
+          bitcoin_address.update_balance
+        end
       end
     end
 
