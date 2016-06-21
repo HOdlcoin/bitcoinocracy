ede00807f7543ffa7061e09107be6e3491bf71a0
FreeTrade <FreeTrade@users.noreply.github.com>
2016-06-21 19:47:31 +0700

    Update signature_validator.rb

--- app/helpers/signature_validator.rb
+++ app/helpers/signature_validator.rb
@@ -4,9 +4,9 @@ class SignatureValidator < ActiveModel::Validator
 
   	else
   		if sig.signature_address == false
-  			sig.errors[:base] << "Invalid signature! Please use bitcoin software to sign the message."
+  			sig.errors[:base] << "Invalid signature! Please use HOdlcoin software to sign the message."
   		else
-      		sig.errors[:base] << "Signature not accepted. Either it is invalid or your bitcoin address (#{sig.signature_address}) is empty."
+      		sig.errors[:base] << "Signature not accepted. Either it is invalid or your HOdlcoin address (#{sig.signature_address}) is empty."
       	end
   	end
   end
